a2ec70b2c5c6c6b5c1280181bb3d43a0f28ac3fd
sharkycake <3084213727@qq.com>
2023-03-09 21:38:45 +0800

    链表的插入操作

--- 数据结构与算法学习/线性表/Linklist.c
+++ 数据结构与算法学习/线性表/Linklist.c
@@ -7,6 +7,8 @@
 // 常用头指针来标识一个单链表，为了操作上的方便，在单链表的第一个结点之前附加一个节点，称为头结点
 
 //-----------带头结点的单链表-------------
+//后插插入操作的时间复杂度为O(n)
+
 
 typedef struct  LNode{
     int data;//数据域
@@ -22,3 +24,31 @@ int InitList(LinkList L){
     L->next = NULL; //将头结点初始化为NULL
     return 1;
 }//初始化一个带头结点的单链表
+
+int ListInsert(LinkList L,int i, int e){
+    //i是要插入的元素的位序,从一开始
+    //首先判断插入的位置是否合法
+    if(i<1){
+        return 0;
+    }
+    LNode *p = L;   //定义一个指针元素指向头结点
+    int j = 0;      //代表当前节点
+    while (p!=NULL && j<i-1)    //找到要插入位序的前驱节点 前驱节点不能是NULL
+    {
+        p = p->next;
+        j++;
+    }
+    if(p == NULL){  //以防p因为空节点而跳出上述循环，保证上述循环可以找到真正的i的前驱节点
+        return 0;
+    }
+    LNode* new = (LNode *)malloc(sizeof(LNode));//创建的是结点,返回的是节点的指针
+    if(!new){
+        return 0;
+    }  //内存空间分配失败
+    new->data = e;  
+    new->next = p->next;
+    p->next = new;
+    return 1;       //创建成功
+}//后插法插入节点
+
+//前插发的插入操作和后插法一致，只需要将两者元素交换一下顺序即可
