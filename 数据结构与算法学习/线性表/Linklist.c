e892d5b9feefe1d04cc45bf510c8927419dda4da
sharkycake <3084213727@qq.com>
2023-03-12 11:20:21 +0800

    新增了删除操作，分离出了GetElem操作

--- 数据结构与算法学习/线性表/Linklist.c
+++ 数据结构与算法学习/线性表/Linklist.c
@@ -7,7 +7,8 @@
 // 常用头指针来标识一个单链表，为了操作上的方便，在单链表的第一个结点之前附加一个节点，称为头结点
 
 //-----------带头结点的单链表-------------
-//后插插入操作的时间复杂度为O(n)
+//后插插入操作的时间复杂度为O(n)，删除操作的时间复杂度为O(n)
+//主要的实践操作都耗费在查找上面，真正对节点插入删除的操作的时间复杂度为O(1)
 
 
 typedef struct  LNode{
@@ -25,30 +26,49 @@ int InitList(LinkList L){
     return 1;
 }//初始化一个带头结点的单链表
 
-int ListInsert(LinkList L,int i, int e){
+LNode * GetLinkListNode(LinkList L,int i){
     //i是要插入的元素的位序,从一开始
     //首先判断插入的位置是否合法
     if(i<1){
-        return 0;
+        return NULL;
     }
-    LNode *p = L;   //定义一个指针元素指向头结点
-    int j = 0;      //代表当前节点
-    while (p!=NULL && j<i-1)    //找到要插入位序的前驱节点 前驱节点不能是NULL
+    LNode *p = L->next;   //定义一个指针元素指向第一个节点(并非头结点)
+    int j = 1;      //代表当前节点
+    while (p!=NULL && j<i)    //找到所需要查找的节点
     {
         p = p->next;
         j++;
     }
     if(p == NULL){  //以防p因为空节点而跳出上述循环，保证上述循环可以找到真正的i的前驱节点
-        return 0;
+        return NULL;
     }
-    LNode* new = (LNode *)malloc(sizeof(LNode));//创建的是结点,返回的是节点的指针
-    if(!new){
-        return 0;
-    }  //内存空间分配失败
-    new->data = e;  
-    new->next = p->next;
-    p->next = new;
-    return 1;       //创建成功
-}//后插法插入节点
+    return p;       //查找成功，返回指针p
+}//按位序查找结点元素
 
-//前插发的插入操作和后插法一致，只需要将两者元素交换一下顺序即可
+int ListInsert(LinkList L,int i, int e){
+    LNode *p = GetLinkListNode(L,i-1);      //查找第i个节点的前驱元素
+    if(p != NULL){      //查找成功
+        LNode* new = (LNode *)malloc(sizeof(LNode));//创建的是结点,返回的是指向这个节点的指针
+        if(!new){
+            return 0;
+        }  //内存空间分配失败
+        new->data = e;  
+        new->next = p->next;
+        p->next = new;
+        return 1;       //创建成功
+    }
+    
+}//后插法插入节点，//前插法的插入操作和后插法一致，只需要将两者元素交换一下顺序即可
+
+int DeleteLinkListNode(LinkList L,int i,int e){
+    if(i<1){
+        return NULL;
+    }
+    LNode * p = GetLinkListNode(L,i-1); //找到第i-1个节点，即被删除节点的前驱节点
+    e = p->next->data;    //将被删除元素返还出去
+    LNode * q = p->next;
+    p->next = q->next;
+    free(q);
+    return 1;       //删除成功
+}//删除位序为i的节点，并return出去
+
